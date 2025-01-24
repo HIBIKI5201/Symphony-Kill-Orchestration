35cc5981f6bcc774d4e6eb0e08cc6374b2cc15cc
HIBIKI_5201 <takuto5201@gmail.com>
2025-01-25 06:08:30 +0900

    [update]クリックした場所に兵士を移動させるように

--- Assets/Resources/Scripts/Master/InGame/SoldierManager.cs
+++ Assets/Resources/Scripts/Master/InGame/SoldierManager.cs
@@ -24,13 +24,16 @@ namespace Orchestration.InGame
         {
             if (_agent != null)
             {
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-
-                if (Physics.Raycast(ray, out hit))
+                if (Input.GetMouseButtonDown(0))
                 {
-                    Vector3 hitPosition = hit.point;
-                    _agent.SetDestination(hitPosition);
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        Vector3 hitPosition = hit.point;
+                        _agent.SetDestination(hitPosition);
+                    }
                 }
             }
         }
