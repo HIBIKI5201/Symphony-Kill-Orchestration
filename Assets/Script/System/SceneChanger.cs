08f2c521622a3a0ea846084e92f05b819ddd0fbb
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-03 01:39:30 +0900

    [update]シーンのロード画面を表示するように

--- Assets/Script/System/SceneChanger.cs
+++ Assets/Script/System/SceneChanger.cs
@@ -1,30 +1,42 @@
 using SymphonyFrameWork.CoreSystem;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Orchestration
 {
     public class SceneChanger
     {
-        private SceneEnum _currentScene;
+        private string _currentSceneName;
+
+        public SceneChanger()
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            _currentSceneName = scene.name;
+        }
 
         public async void SceneLoad(SceneEnum sceneEnum)
         {
             FadeOut();
 
             //ロードシーンのロード
-            if (await SceneLoader.LoadScene(SceneEnum.LoadingScene.ToString()))
+            if (!await SceneLoader.LoadScene(SceneEnum.LoadingScene.ToString()))
             {
                 Debug.LogError("ロードシーンを読み込めませんでした");
                 return;
             }
 
+            Scene? scene = SceneLoader.GetExistScene(SceneEnum.LoadingScene.ToString());
+
+            FadeIn();
+
             //元のシーンをアンロード
-            bool unloadSuccess = await SceneLoader.UnloadScene(_currentScene.ToString(),
+            bool unloadSuccess = await SceneLoader.UnloadScene(_currentSceneName.ToString(),
                 progress =>
                 {
                     ProgressBarUpdate(progress / 2); //0～50%の範囲
                 });
 
+            await Awaitable.WaitForSecondsAsync(5);
 
             if (unloadSuccess)
             {
@@ -38,16 +50,23 @@ namespace Orchestration
 
                 if (loadSuccess)
                 {
-                    //ロードに成功したら現在のシーンのEnumを記憶する
-                    _currentScene = sceneEnum;
+                    //ロードに成功したら現在のシーンをアクティブ化して記憶する
+                    Scene? scene = SceneLoader.GetExistScene(sceneEnum.ToString());
+                    if (scene.HasValue)
+                    {
+                        SceneManager.SetActiveScene(scene.Value);
+                        _currentSceneName = sceneEnum.ToString();
+                    }
                 }
                 else
                 {
-                    //ロードに失敗した場合はホームに帰る
+                    //ロードに失敗した場合はホームに戻る
                     await SceneLoader.LoadScene(SceneEnum.Home.ToString());
                 }
             }
             else return;
+
+            FadeOut();
 
             //ロードシーンをアンロード
             await SceneLoader.UnloadScene(SceneEnum.LoadingScene.ToString());
