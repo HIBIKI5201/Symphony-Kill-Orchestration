67b89e1f99230d58c04d0a96a6ce0586aa8a83c0
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-03 04:36:17 +0900

    [add]デバッグ機能のアセンブリを分割

--- Assets/Script/Debug/DestoryObject.cs
+++ Assets/Script/Debug/DestoryObject.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 
-namespace Orchestration
+namespace Orchestration.DebugFunction
 {
     public class DestoryObject : MonoBehaviour
     {
         [SerializeField]
         private float _destroyDeferment = 0;
-
         void Start() => Destroy(gameObject, _destroyDeferment);
     }
 }
