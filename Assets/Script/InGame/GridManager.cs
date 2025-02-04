9c9088ddec1cfc3d4289bac3cd815c7191bd6b36
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-04 16:59:25 +0900

    [update]チャンクの自動生成を実装

--- Assets/Script/InGame/GridManager.cs
+++ Assets/Script/InGame/GridManager.cs
@@ -2,6 +2,7 @@ using SymphonyFrameWork.CoreSystem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Unity.AI.Navigation;
 using UnityEngine;
 using UnityEngine.AI;
@@ -14,6 +15,8 @@ namespace Orchestration.InGame
     /// </summary>
     public class GridManager : MonoBehaviour
     {
+        private const string GridPrefabsName = "Grid Prefabs";
+
         [SerializeField, Tooltip("グリッドの大きさ")]
         private float _gridSize = 1f;
 
@@ -29,12 +32,21 @@ namespace Orchestration.InGame
         [SerializeField, Tooltip("グリッドが選ばれている時のマテリアル")]
         private Material _selectGridMaterial;
 
-
         private List<GridInfo> _griInfoList = new();
 
         private GridInfo _highLightingGrid;
 
         private Vector3 _originPosition;
+
+
+        [Space]
+        [SerializeField]
+        private GameObject _chunkPrefab;
+
+        private Queue<GameObject> _chunkQueue = new();
+
+        private float _lastChunkPos = 0;
+
         private void OnEnable()
         {
             ServiceLocator.SetInstance(this);
@@ -45,15 +57,49 @@ namespace Orchestration.InGame
             ServiceLocator.DestroySingleton<GridManager>();
         }
 
-        private void Start()
+        private async void Start()
         {
-            GetComponent<NavMeshSurface>().BuildNavMesh();
+            NavMeshSurface[] surfaces = GetComponentsInChildren<NavMeshSurface>();
 
+            foreach (var surface in surfaces)
+            {
+                await ChunkBuild(surface);
+            }
+        }
+
+        [ContextMenu("New Chunk Create")]
+        public async void NewChunkCreate()
+        {
+            GameObject chunk = Instantiate(_chunkPrefab, new Vector3(_lastChunkPos, 0, 0), Quaternion.identity);
+
+            if (chunk.TryGetComponent<NavMeshSurface>(out var surface))
+            {
+                await ChunkBuild(surface);
+            }
+        }
+
+        private async Task ChunkBuild(NavMeshSurface surface)
+        {
+            _lastChunkPos += 10;
+            surface.BuildNavMesh();
+
+            //グリッドの位置をリスト化
             List<Vector3> gridPosList = GridCreate();
 
+            gridPosList = FilterNonexistentGrid(gridPosList);
+
+            //グリッドを生成
             if (_gridPrefab)
             {
-                GridPrefabInstantiate(gridPosList);
+                await GridPrefabInstantiate(gridPosList, surface.transform);
+            }
+
+            _chunkQueue.Enqueue(surface.gameObject);
+
+            if (_chunkQueue.Count > 3)
+            {
+                GameObject chunk = _chunkQueue.Dequeue();
+                Destroy(chunk);
             }
         }
 
@@ -89,31 +135,59 @@ namespace Orchestration.InGame
         }
 
         /// <summary>
+        /// グリッドが既に生成されている位置を除外する
+        /// </summary>
+        /// <param name="list">フィルターしたい座標のリスト</param>
+        /// <returns>フィルター済のリスト</returns>
+        private List<Vector3> FilterNonexistentGrid(List<Vector3> list)
+        {
+            //検索の高速化のためにHashSetに変換
+            HashSet<Vector3> filter = new(_griInfoList.Select(gi => gi.transform.position));
+
+            List<Vector3> filtered = new();
+
+            foreach (var pos in list)
+            {
+                if (!filter.Contains(pos))
+                {
+                    filtered.Add(pos);
+                }
+            }
+
+            return filtered;
+        }
+
+        /// <summary>
         /// グリッドの位置にプレハブを生成
         /// </summary>
         /// <param name="list">グリッドの位置</param>
-        private async void GridPrefabInstantiate(List<Vector3> list)
+        private async Task GridPrefabInstantiate(List<Vector3> list, Transform parent)
         {
-            _griInfoList.Clear();
+            GameObject rootObj = new(GridPrefabsName);
 
             //プレハブの親オブジェクトを生成
-            GameObject rootObj = new GameObject("GridPrefabs");
-            rootObj.transform.parent = transform;
+            rootObj.transform.parent = parent;
 
-            //親オブジェクトの子としてグリッドプレハブを一括生成
-            GameObject[] objects = await InstantiateAsync(original: _gridPrefab,
-                count: list.Count,
-                parent: rootObj.transform,
-                positions: new Span<Vector3>(list.ToArray()),
-                rotations: new Span<Quaternion>(Enumerable.Repeat(Quaternion.identity, list.Count).ToArray()));
 
-            //生成したオブジェクトのGridInfoを取得
-            for (int i = 0; i < objects.Length; i++)
+            if (list.Count > 0)
             {
-                _griInfoList.Add(
-                    objects[i].TryGetComponent<GridInfo>(out var info) ?
-                    info : objects[i].AddComponent<GridInfo>()
-                    );
+                //親オブジェクトの子としてグリッドプレハブを一括生成
+                GameObject[] objects = await InstantiateAsync(original: _gridPrefab,
+                    count: list.Count,
+                    parent: rootObj.transform,
+                    positions: new Span<Vector3>(list.ToArray()),
+                    rotations: new Span<Quaternion>(Enumerable.Repeat(Quaternion.identity, list.Count).ToArray()));
+
+                //生成したオブジェクトのGridInfoを取得
+                for (int i = 0; i < objects.Length; i++)
+                {
+                    GameObject obj = objects[i];
+                    _griInfoList.Add(
+                        obj.TryGetComponent<GridInfo>(out var info) ?
+                        info : obj.AddComponent<GridInfo>()
+                        );
+                    obj.transform.localScale = Vector3.one * _gridSize;
+                }
             }
         }
 
