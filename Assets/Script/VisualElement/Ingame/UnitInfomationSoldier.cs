56c951f032c15199b9bf1157ece1fe588e9d73af
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-04 14:32:54 +0900

    [add]兵士インフォのクラスを追加

---
+++ Assets/Script/VisualElement/Ingame/UnitInfomationSoldier.cs
@@ -0,0 +1,35 @@
+using SymphonyFrameWork.Utility;
+using System.Threading.Tasks;
+using UnityEngine.UIElements;
+
+namespace Orchestration.UI
+{
+    [UxmlElement]
+    public partial class UnitInfomationSoldier : SymphonyVisualElement
+    {
+        private Label _name;
+        private VisualElement _healthBar;
+
+        public UnitInfomationSoldier() : base("UXML/Ingame/HUD/UnitInfomation-Soldier", Position.Relative) { }
+        protected override Task Initialize_S(TemplateContainer container)
+        {
+            this.style.maxHeight = Length.Percent(30);
+            this.style.minHeight = Length.Percent(15);
+
+            _name = container.Q<Label>("name");
+            _healthBar = container.Q<VisualElement>("health-bar");
+
+            return Task.CompletedTask;
+        }
+
+        public void SetName(string name)
+        {
+            _name.text = name;
+        }
+
+        public void HealthBarUpdate(float proportion)
+        {
+            _healthBar.style.width = Length.Percent(proportion * 100);
+        }
+    }
+}
