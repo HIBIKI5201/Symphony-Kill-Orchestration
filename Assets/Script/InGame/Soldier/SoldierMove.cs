f990192825ba0963d9c9addb9d21a777fc6b8a3a
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-05 06:18:35 +0900

    [update]回転処理を別メソッドに分離

--- Assets/Script/InGame/Soldier/SoldierMove.cs
+++ Assets/Script/InGame/Soldier/SoldierMove.cs
@@ -15,15 +15,16 @@ namespace Orchestration.Entity
         private Animator _animator;
         public Animator Animator { get => _animator; }
 
-        [SerializeField]
-        private Transform _target;
-        public Transform Target { get => _target; }
-
         private Vector2 _currentDirection;
 
         private void Awake()
         {
             _agent = GetComponent<NavMeshAgent>();
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        public void Init()
+        {
             if (_agent.NullCheckComponent("NavMeshAgentが見つかりません"))
             {
                 //手動で動かすためアップデートはなし
@@ -33,39 +34,41 @@ namespace Orchestration.Entity
                 _agent.autoTraverseOffMeshLink = true;
             }
 
-            _animator = GetComponentInChildren<Animator>();
             if (_animator.NullCheckComponent("Animatorが見つかりません"))
             {
                 _animator.applyRootMotion = false;
-            }
-
-            if (_target.NullCheckComponent("ターゲットが見つかりません"))
-            {
-                _target.parent = null;
             }
         }
 
         public void Move()
         {
+            //ターゲットのベクトルを計算
             Vector3 localNextPos = transform.InverseTransformPoint(_agent.nextPosition);
-            Vector2 targetDirection = new Vector2(localNextPos.x, localNextPos.z).normalized;
+            Vector2 direction = new Vector2(localNextPos.x, localNextPos.z).normalized;
 
             //Lerpで滑らかに変化
-            _currentDirection = Vector2.Lerp(_currentDirection, targetDirection, Time.deltaTime * 5);
+            _currentDirection = Vector2.Lerp(_currentDirection, direction, Time.deltaTime * 3);
 
             _animator.SetFloat("Right", _currentDirection.x);
             _animator.SetFloat("Forward", _currentDirection.y);
 
             //自身の位置をAgentに同期
             transform.position = _agent.nextPosition;
+        }
 
-            Vector3 direction = _target.position - transform.position;
-            direction.y = 0;  // Y軸方向を無視
+        public Vector3 TargetDirection()
+        {
+            return _agent.velocity.normalized;
+        }
 
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-
-            // Lerpで滑らかに変化
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * 5);
+        public void Rotation(Vector3 direction)
+        {
+            // 進行方向がある場合のみ回転
+            if (direction.magnitude > 0.1f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 3f);
+            }
         }
 
         public void SetDirection()
