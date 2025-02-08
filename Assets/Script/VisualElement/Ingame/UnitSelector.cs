dc41a1f31df428b49da1254dad2c69ef56ddda1e
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-08 20:07:39 +0900

    [update]セレクターに情報を追加する機能を実装

--- Assets/Script/VisualElement/Ingame/UnitSelector.cs
+++ Assets/Script/VisualElement/Ingame/UnitSelector.cs
@@ -1,4 +1,5 @@
 using SymphonyFrameWork.Utility;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,10 +9,25 @@ namespace Orchestration.UI
     [UxmlElement]
     public partial class UnitSelector : SymphonyVisualElement
     {
+        private VisualElement _panel;
+
+        private List<UnitSelectorSoldier> _soldierList = new();
+
         public UnitSelector() : base("UXML/Ingame/HUD/UnitSelector") { }
         protected override Task Initialize_S(TemplateContainer container)
         {
+            _panel = container.Q<VisualElement>("SelectPanel");
+
             return Task.CompletedTask;
+        }
+
+        public void AddSoldierInfo(UnitSelectorSoldier info)
+        {
+            if (!_soldierList.Contains(info))
+            {
+                _soldierList.Add(info);
+                _panel.Add(info);
+            }
         }
     }
 }
