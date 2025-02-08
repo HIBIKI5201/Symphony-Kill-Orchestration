147e19a45ef5152aef566b9f93e2dc96fd29afb5
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-08 20:06:38 +0900

    [update]セレクターインフォに初期化情報を追加

--- Assets/Script/VisualElement/Ingame/UnitSelectorSoldier.cs
+++ Assets/Script/VisualElement/Ingame/UnitSelectorSoldier.cs
@@ -8,6 +8,10 @@ namespace Orchestration.UI
     [UxmlElement]
     public partial class UnitSelectorSoldier : SymphonyVisualElement
     {
+        private VisualElement _icon;
+        private VisualElement _specialPointGuage;
+        private Label _specialPointCount;
+
         public UnitSelectorSoldier() : base("UXML/Ingame/HUD/UnitSelector-Soldier", InitializeType.FullRangth) { }
 
         protected override Task Initialize_S(TemplateContainer container)
@@ -16,6 +20,21 @@ namespace Orchestration.UI
             this.style.minWidth = Length.Percent(15);
 
             return Task.CompletedTask;
+        }
+
+        public void Init(Texture2D texture)
+        {
+            _icon.style.backgroundImage = new StyleBackground(texture);
+        }
+
+        public void UpdateSpecialPointInfo(float proportion, int count)
+        {
+
+        }
+
+        public void Destroy()
+        {
+            RemoveFromHierarchy();
         }
     }
 }
