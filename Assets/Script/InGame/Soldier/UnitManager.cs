ea4c66f9b0547f60d5d536910365d5c4dfe9515b
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-09 17:35:04 +0900

    [update]兵士を開始時に生成するように変更

--- Assets/Script/InGame/Soldier/UnitManager.cs
+++ Assets/Script/InGame/Soldier/UnitManager.cs
@@ -18,15 +18,30 @@ namespace Orchestration.InGame
 
         private List<PlayerSoldierManager> _soldiers = new();
 
-        public GameObject GetSoldierPrefabByType(SoldierType soldierType)
+        private void Start()
         {
-            return soldierType switch {
+            AddSoldier(SoldierType.Asult, new Vector3(0.5f, 0, -3.5f));
+            AddSoldier(SoldierType.Support, new Vector3(0.5f, 0, -1.5f));
+            AddSoldier(SoldierType.Medic, new Vector3(0.5f, 0, 1.5f));
+            AddSoldier(SoldierType.Recon, new Vector3(0.5f, 0, 3.5f));
+        }
+
+        public GameObject GetSoldierPrefabByType(SoldierType type)
+        {
+            return type switch {
                 SoldierType.Asult => _asult,
                 SoldierType.Medic => _medic,
                 SoldierType.Support => _support,
                 SoldierType.Recon => _recon,
                 _ => null,
             };
+        }
+
+        public void AddSoldier(SoldierType type, Vector3 position)
+        {
+            GameObject prefab = GetSoldierPrefabByType(type);
+            prefab = Instantiate(prefab, position, Quaternion.identity);
+            prefab.transform.parent = transform;
         }
 
         public enum SoldierType
