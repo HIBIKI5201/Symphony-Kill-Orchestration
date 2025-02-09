d3844fdac986fd4dd3c2c993e1551ce10b8ade44
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-09 16:49:29 +0900

    [update]ミニマップを移動するように

--- Assets/Script/InGame/IngameUIManager.cs
+++ Assets/Script/InGame/IngameUIManager.cs
@@ -13,6 +13,13 @@ namespace Orchestration.InGame
         private UnitInfomation _unitInfo;
         private UnitSelector _unitSelector;
 
+        private Transform _miniMapCamera;
+
+        [SerializeField]
+        private float _miniMapCameraSpeed = 1;
+
+        private float _miniMapFirstPosX;
+
         private void Awake()
         {
             _document = GetComponent<UIDocument>();
@@ -23,6 +30,15 @@ namespace Orchestration.InGame
                 _unitInfo = root.Q<UnitInfomation>();
                 _unitSelector = root.Q<UnitSelector>();
             }
+
+            _miniMapCamera = GetComponentInChildren<Camera>().transform;
+            _miniMapFirstPosX = _miniMapCamera.position.x;
+        }
+
+        private void Start()
+        {
+            IngameSystemManager system = ServiceLocator.GetInstance<IngameSystemManager>();
+            system.OnStageChanged += MoveMiniMapCamera;
         }
 
         private void OnEnable()
@@ -37,5 +53,21 @@ namespace Orchestration.InGame
 
         public void AddSoldierInfo(UnitInfomationSoldier info) => _unitInfo?.AddSoldierInfo(info);
         public void AddSoldierSelector(UnitSelectorSoldier info) => _unitSelector?.AddSoldierInfo(info);
+
+        private async void MoveMiniMapCamera(int count)
+        {
+            float nextPosX = count * 10 + _miniMapFirstPosX;
+
+            //次のステージ位置に移動するまで繰り返す
+            while (nextPosX >= _miniMapCamera.position.x)
+            {
+                _miniMapCamera.position += new Vector3(_miniMapCameraSpeed * Time.deltaTime, 0, 0);
+
+                await Awaitable.NextFrameAsync();
+            }
+
+            //移動完了したら整数値に戻す
+            _miniMapCamera.position = new Vector3(nextPosX, _miniMapCamera.position.y, _miniMapCamera.position.z);
+        }
     }
 }
