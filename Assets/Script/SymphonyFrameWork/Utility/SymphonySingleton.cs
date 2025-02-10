b286c97607338a78e820fee7e7eea204cc76923b
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-10 19:28:58 +0900

    [update]SymphonyFrameWorkの更新

--- Assets/Script/SymphonyFrameWork/Utility/SymphonySingleton.cs
+++ Assets/Script/SymphonyFrameWork/Utility/SymphonySingleton.cs
@@ -1,4 +1,6 @@
 using SymphonyFrameWork.CoreSystem;
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace SymphonyFrameWork.Utility
@@ -10,12 +12,36 @@ namespace SymphonyFrameWork.Utility
 
         private void OnEnable()
         {
-            ServiceLocator.SetInstance(_target);
+            if (_target)
+            {
+                //Targetのクラスをキャストして実行する
+                Type targetType = _target.GetType();
+                MethodInfo method = typeof(ServiceLocator).GetMethod(nameof(ServiceLocator.SetInstance))
+                    .MakeGenericMethod(targetType);
+
+                method.Invoke(null, new object[] { _target, ServiceLocator.LocateType.Locator });
+            }
+        }
+
+        private void Start()
+        {
+            var a = ServiceLocator.GetInstance<Camera>();
+            Debug.Log(a.name);
         }
 
         private void OnDisable()
         {
-            ServiceLocator.DestroyInstance(_target);
+            if (_target)
+            {
+                //Targetのクラスをキャストして実行する
+                Type targetType = _target.GetType();
+                MethodInfo method = typeof(ServiceLocator)
+                    .GetMethod(nameof(ServiceLocator.DestroyInstance),
+                    BindingFlags.Static | BindingFlags.Public, null,
+                    new Type[] { typeof(Component) }, null);
+
+                method.Invoke(null, new object[] { _target });
+            }
         }
     }
 }
