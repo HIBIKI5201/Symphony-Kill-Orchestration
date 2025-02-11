0c278620aa68c1abd35c31aa2ccd13d14b43beca
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-11 22:56:02 +0900

    [update]他のスキルのCSも追加

---
+++ Assets/Script/InGame/Soldier/Player/Skill/ReconSkill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Orchestration.Entity
+{
+    public class ReconSkill : SkillBase
+    {
+        public override void SkillActive(PlayerSoldierManager soldier) => throw new global::System.NotImplementedException();
+    }
+}
---
+++ Assets/Script/InGame/Soldier/Player/Skill/SpportSkill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Orchestration.Entity
+{
+    public class SpportSkill : SkillBase
+    {
+        public override void SkillActive(PlayerSoldierManager soldier) => throw new global::System.NotImplementedException();
+    }
+}
