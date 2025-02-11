4f0f72afe7a128319d0bfa3bbd58d826338e4b17
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-11 18:32:20 +0900

    [update]チャンクのアセットコンポーネントを追加

---
+++ Assets/Script/InGame/Ground/ChunkAsset.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Orchestration
+{
+    public class ChunkAsset : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject _enemy;
+
+        [SerializeField]
+        GameObject _object;
+    }
+}
