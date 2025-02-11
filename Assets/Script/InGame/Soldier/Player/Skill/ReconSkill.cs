0d59800b0033f5c01cc521b8a45daeb539af54d1
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-12 04:05:02 +0900

    [update]リコンのスキルを追加

--- Assets/Script/InGame/Soldier/Player/Skill/ReconSkill.cs
+++ Assets/Script/InGame/Soldier/Player/Skill/ReconSkill.cs
@@ -4,10 +4,21 @@ namespace Orchestration.Entity
 {
     public class ReconSkill : SkillBase
     {
+        [SerializeField]
+        private LayerMask _target;
         protected override bool SkillProccess(PlayerSoldierManager soldier, SoldierData_SO data)
         {
             Debug.Log("ƒŠƒRƒ“ƒXƒLƒ‹”­“®");
-            return true;
+
+            var attackModule = GetComponent<SoldierAttack>();
+
+            if (attackModule.SearchTarget(data.AttackRange, _target, out var s))
+            {
+                attackModule.AttackEnemy(s, data.Attack * 5, soldier);
+                return true;
+            }
+
+            return false;
         }
     }
 }
