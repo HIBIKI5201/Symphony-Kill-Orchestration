cfe28e5a7703abb724b43c2f88098dfd1d8143f0
HIBIKI_5201 <takuto5201@gmail.com>
2025-02-13 10:30:13 +0900

    [update]アサルトスキルにも範囲表示を追加

--- Assets/Script/InGame/Soldier/Player/Skill/AssaultSkill.cs
+++ Assets/Script/InGame/Soldier/Player/Skill/AssaultSkill.cs
@@ -20,6 +20,35 @@ namespace Orchestration.Entity
         [SerializeField]
         GameObject _particle;
 
+        [SerializeField]
+        private GameObject _rangePrefab;
+
+        private void Start()
+        {
+            _rangePrefab = Instantiate(_rangePrefab, transform.position, Quaternion.identity, transform);
+            _rangePrefab.SetActive(false);
+
+            var manager = GetComponent<SoldierManager>();
+
+            if (manager)
+            {
+                float range = manager.Data.AttackRange;
+                _rangePrefab.transform.localScale = new Vector3(range * 2, _rangePrefab.transform.localScale.y, range * 2);
+            }
+        }
+
+        public override void SkillVisible()
+        {
+            _rangePrefab?.SetActive(true);
+        }
+
+        public override void SkillActive(PlayerSoldierManager soldier, SoldierData_SO data)
+        {
+            _rangePrefab?.SetActive(false);
+            base.SkillActive(soldier, data);
+        }
+
+
         protected override bool SkillProccess(PlayerSoldierManager soldier, SoldierData_SO data)
         {
             Collider[] colliders = Physics.OverlapSphere(transform.position, data.AttackRange, _target);
@@ -52,7 +81,7 @@ namespace Orchestration.Entity
                 //ï∫émÇ…É_ÉÅÅ[ÉWÇó^Ç¶ÇÈ
                 foreach (var s in soldiers)
                 {
-                    attackModule.AttackEnemy(s, new (data.Attack), soldier);
+                    attackModule.AttackEnemy(s, new(data.Attack), soldier);
                     Instantiate(_particle,
                         s.transform.position + Vector3.one * UnityEngine.Random.Range(0f, 0.5f),
                         Quaternion.identity);
